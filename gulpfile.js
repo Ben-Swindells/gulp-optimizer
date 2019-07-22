c0a765ac05f942f1e646053b4aff4fcab571fc34
Ben Swindells <Ben.Swindells@southbankresearch.com>
2019-07-22 11:38:05 +0100

    Added Image Optimzer

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { watch } = require('gulp');
 cssbeautify = require('gulp-cssbeautify');
 let cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function () {
     return gulp.src('components/scss/styles.scss') // Get source files with gulp.src
@@ -15,6 +16,22 @@ gulp.task('clean', function() {
         .pipe(cssbeautify())
         .pipe(gulp.dest('components/cleaned-css'));
 });
+gulp.task('imageMin', () =>
+    gulp.src('components/images/*')
+        .pipe(imagemin())
+        .pipe(imagemin([
+          imagemin.gifsicle({interlaced: true}),
+          imagemin.jpegtran({progressive: true}),
+          imagemin.optipng({optimizationLevel: 5}),
+          imagemin.svgo({
+              plugins: [
+                  {removeViewBox: true},
+                  {cleanupIDs: false}
+              ]
+          })
+      ]))
+        .pipe(gulp.dest('components/images-compressed'))
+);
 
   gulp.task('watch' , function(){
     watch('components/scss/styles.scss', gulp.series('sass')); 
